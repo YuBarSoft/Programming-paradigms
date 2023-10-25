f61bd4ef00ab9613d13c00e6a2376b35c28a7b3d
Iurii Baranov <111112727+YuBarSoft@users.noreply.github.com>
2023-10-25 17:40:26 +0300

    Update task-1.py

--- HW-1/task-1.py
+++ HW-1/task-1.py
@@ -1,1 +1,16 @@
+# Задача №1
+# Дан список целых чисел numbers. Необходимо написать в императивном стиле процедуру для
+# сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки.
 
+def reverse_sort(lst):
+    nums = len(lst)
+    for i in range(nums):
+        for j in range(nums - 1 - i):
+            if lst[j] < lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+    return lst
+
+
+init_list = [1, 2, 6, 3, 8, 4, 2, 1]
+print(reverse_sort(init_list))
